ae22c68ef98dc4c3f127eb63f070b9ce98b07ce4
AsadullahBadhan <asadullah.badhan@gmail.com>
2020-08-09 09:03:30 +0600

    wood calculator bug fixed

--- assignment.js
+++ assignment.js
@@ -1,26 +1,3 @@
-// 1.feetToMile(feet){
-// }
-// 2.woodCalculator(chiar, table, khat){
-// return total amount of wood.
-
-// chair = 1;
-// table = 3;
-// khat = 5;
-// }
-
-// 3.brickCalculator(floor){
-// return total amount brick needed for the building
-
-// frist10 = 15feet;
-// second10 = 12feet;
-// rest = 10feet;
-// each feet need 1000 brick;
-// }
-
-// 4.tinyFriend(array){
-// array of string will given return the smallest string;
-// }
-
 function feetToMile(feet) {
     if (feet < 0) {
         return 'Distance can not be a nagetive number';
@@ -31,11 +8,16 @@ function feetToMile(feet) {
 
 // console.log(feetToMile(5893));
 
-function woodCalculator(chiar, table, bed) {
+function woodCalculator(chair, table, bed) {
     var totalWood = 0;
-    totalWood = chiar + table * 3 + bed * 5;
+    if (chair < 0 || table < 0 || bed < 0) {
+        return "Quantity can not be nagetive number"
+    } else {
+        totalWood = chair + table * 3 + bed * 5;
+    }
     return totalWood;
 }
 
-var result = woodCalculator(1, 5, 1);
-console.log(result);
+// var result = woodCalculator(20, 5, 10);
+// console.log(result);
+
