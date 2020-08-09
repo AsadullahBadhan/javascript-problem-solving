76ed87cfa8c3190d7eb32b02a3dd77d182221088
AsadullahBadhan <asadullah.badhan@gmail.com>
2020-08-09 09:40:05 +0600

    brick calculator created

--- assignment.js
+++ assignment.js
@@ -1,9 +1,11 @@
 function feetToMile(feet) {
+    var mile = 0;
     if (feet < 0) {
         return 'Distance can not be a nagetive number';
     } else {
-        return feet / 5280;
+        mile = feet / 5280;
     }
+    return mile;
 }
 
 // console.log(feetToMile(5893));
@@ -21,3 +23,23 @@ function woodCalculator(chair, table, bed) {
 // var result = woodCalculator(20, 5, 10);
 // console.log(result);
 
+function brickCalculator(floorNumber) {
+    var height = 0;
+    if (floorNumber < 0) {
+        return "Floor number can not be a nagetive number"
+    }
+    else if (floorNumber <= 10) {
+        height = floorNumber * 15;
+    }
+    else if (floorNumber <= 20) {
+        height = (floorNumber - 10) * 12 + 150;
+    }
+    else {
+        height = (floorNumber - 20) * 10 + 270;
+    }
+
+    return height * 1000;
+}
+
+var brick = brickCalculator(14);
+console.log(brick);
